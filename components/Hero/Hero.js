84d38159d76b45362824e933a06b431e201139b2
Dennis Acosta <email@dennisacosta.me>
2022-02-21 20:26:43 -0500

    Add hero buttons

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import Heading from '../Heading';
 import BodyText from '../BodyText';
+import Button from '../Button';
 
 import heroStyles from './Hero.module.scss';
 
@@ -19,6 +20,20 @@ const Hero = () => {
                     membership is purchased through an NFT and owned by the
                     token-holders.
                 </BodyText>
+                <div className={`${heroStyles['hero-btns']}`}>
+                    <div className={`${heroStyles['btn-container']}`}>
+                        <a href='#'>
+                            <Button style='primary-btn'>Become a Member</Button>
+                        </a>
+                    </div>
+                    <div className={`${heroStyles['btn-container']}`}>
+                        <a href='#'>
+                            <Button style='secondary-btn'>
+                                Join the Discord
+                            </Button>
+                        </a>
+                    </div>
+                </div>
             </div>
             <figure className={`${heroStyles['hero-img']}`}>
                 <Image src={heroImg} height='600' width='600' />

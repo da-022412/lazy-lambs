0936cdb5176cb82b6b076c87a89bbc2b8467595e
Dennis Acosta <email@dennisacosta.me>
2022-02-22 10:18:02 -0500

    Convert club to columns

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
 import Hero from '../components/Hero';
 import Information from '../components/Information';
 import Divider from '../components/Divider';
-import Club from '../components/Club';
+import MultiColumn from '../components/MultiColumn';
+
+const club = {
+    title: 'The Club',
+    content: [
+        {
+            heading: 'The Community',
+            copy: 'Lazy Lamb members are shaped by car culture: its history, its technology, its lifestyle, and most importantly, the thrill of the drive. While our car knowledge is the common bond, it is the passions and pursuits of each Member that makes Lazy Lamb so unique, creating the perfect environment for networking and socializing.',
+        },
+        {
+            heading: 'Events & Pop-ups',
+            copy: 'Join this exclusive community for physical & virtual events, including parties, car meetups, charity drives, and other exclusive events that bring our Members together to create a truly special atmosphere and forge friendships.',
+        },
+        {
+            heading: 'Driving Experiences',
+            copy: 'Lazy Lamb members are shaped by car culture: its history, its technology, its lifestyle, and most importantly, the thrill of the drive. While our car knowledge is the common bond, it is the passions and pursuits of each Member that makes Lazy Lamb so unique, creating the perfect environment for networking and socializing.',
+        },
+    ],
+};
 
 export default function Home() {
     return (
@@ -9,7 +27,7 @@ export default function Home() {
             <Hero />
             <Information />
             <Divider />
-            <Club />
+            <MultiColumn content={club} />
             <Divider />
         </>
     );

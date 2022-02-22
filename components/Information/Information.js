dccd63e5edf351c2e8f86735a9c3458e7bdae4bf
Dennis Acosta <email@dennisacosta.me>
2022-02-21 21:28:02 -0500

    Add info section

---
+++ components/Information/Information.js
@@ -0,0 +1,70 @@
+import Image from 'next/image';
+
+import Heading from '../Heading';
+import BodyText from '../BodyText';
+
+import infoStyles from './Info.module.scss';
+
+import imgOne from '../../assets/images/lazy-lamb-black-lambo.webp';
+import imgTwo from '../../assets/images/lazy-lamb-green-lambo.webp';
+import imgThree from '../../assets/images/lazy-lamb-purple-lambo.webp';
+import imgFour from '../../assets/images/lazy-lamb-red-lambo.webp';
+
+const images = [
+    {
+        id: 0,
+        img: imgOne,
+    },
+    {
+        id: 1,
+        img: imgTwo,
+    },
+    {
+        id: 2,
+        img: imgThree,
+    },
+    {
+        id: 3,
+        img: imgFour,
+    },
+];
+
+const Information = () => {
+    return (
+        <section className={`${infoStyles['info-section']}`}>
+            <div className={`${infoStyles['info-container']}`}>
+                <div className={`${infoStyles['info-imgs']}`}>
+                    {images.map((img) => (
+                        <Image
+                            src={img.img}
+                            height={250}
+                            width={250}
+                            key={img.id}
+                        />
+                    ))}
+                </div>
+                <div className={`${infoStyles['info-content']}`}>
+                    <Heading style='heading-3' level='3'>
+                        The project offers 10,000 limited non-fungible
+                        membership tokens with proof of ownership stored on the
+                        Ethereum blockchain.
+                    </Heading>
+                    <BodyText>
+                        Token ownership grants access to our private car club,
+                        with exclusive member-only benefits to experience the
+                        high-end automotive lifestyle, including the chance to
+                        win three real-life supercars!
+                    </BodyText>
+                    <BodyText>
+                        Members can also visit our virtual marketplace to buy,
+                        sell, or trade digital vehicles directly from our
+                        metaverse dealership, and experience them on a virtual
+                        track.
+                    </BodyText>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Information;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from '../components/Hero';
+import Information from '../components/Information';
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <Information />
         </>
     );
 }

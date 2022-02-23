fd964c3c19ae56e54880c931f002d120157839b4
Dennis Acosta <email@dennisacosta.me>
2022-02-22 21:33:48 -0500

    Add link styling

--- pages/index.js
+++ pages/index.js
@@ -102,7 +102,7 @@ const lambs = {
             p: 'In addition to the membership benefits, Each Lazy Lamb NFT also includes a unique digital collectible, officially owned by the token holder. Each Lamb artwork is unique, and no two are the same (And some are very, very special...) The collection includes 10,000 unique collectibles with multiple traits, including different supercar models, colors, accessories, expressions, clothing, and much more.',
         },
         {
-            p: 'The complete collection is stored as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS. View our provenance record.',
+            p: 'The complete collection is stored as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS. <a href="#">View our provenance record.</a>',
         },
     ],
     img: PlaceHolder,

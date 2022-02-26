5f3a300279f17312bf43f829abe37621dafdc1b1
Dennis Acosta <email@dennisacosta.me>
2022-02-26 09:11:18 -0500

    Add list to content

--- pages/index.js
+++ pages/index.js
@@ -115,21 +115,13 @@ const charity = {
             p: 'We’re not just fun and lifestyle. At Lazy Lamb, we’re also committed to giving back something positive, and doing our part to help take care of others.',
         },
         {
-            p: '<b>As part of our roadmap, we will donate:</b>',
+            p: '<b>As part of our roadmap, we will donate:</b><br><br><ul><li><span style="color:#ffea0e">$200,000</span> to Ride2Revive upon hitting 20% of our roadmap</li><li><span style="color:#ffea0e">$600,000</span> to a non-profit organization selected by majority vote from our members upon hitting 60% of our roadmap</li></ul>',
         },
         {
             p: '<b>Ride2Revive</b> is a non-profit organization that has helped and inspired hundreds of children battling life-threatening illnesses for over ten years by giving them a once-in-a-lifetime exhilarating race car experience, bringing miles of smiles to the kids and their families.',
         },
         {
             p: '<b>Ride2Revive</b> benefits children from various hospitals and organizations, including Joe DiMaggio Children’s Hospital, Holtz Children’s Hospital, Nicklaus Children’s Hospital, and more. To learn more about their work and mission, visit ride2revive.org.',
-        },
-    ],
-    list: [
-        {
-            li: '$200,000 to Ride2Revive upon hitting 20% of our roadmap ',
-        },
-        {
-            li: '$600,000 to a non-profit organization selected by majority vote from our members upon hitting 60% of our roadmap ',
         },
     ],
     img: PlaceHolder,

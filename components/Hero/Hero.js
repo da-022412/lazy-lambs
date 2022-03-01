f9cb060897b4fc348fb08fe3a4ab022388b0fabd
Dennis Acosta <email@dennisacosta.me>
2022-03-01 14:56:35 -0500

    Complete hero mobile styles

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -11,26 +11,28 @@ import heroImg from '../../assets/images/lazy-lamb-red-purple.webp';
 
 const Hero = () => {
     return (
-        <Container>
-            <div className={`${heroStyles['hero-container']}`}>
-                <div className={`${heroStyles['heading-container']}`}>
-                    <Heading style='heading-1' level='1'>
-                        The World’s First NFT Car Club & Marketplace
-                    </Heading>
-                    <div className={`${heroStyles['hero-btns']}`}>
-                        <Button style='primary-btn' link='#'>
-                            Become a Member
-                        </Button>
-                        <Button style='secondary-btn' link='#'>
-                            Join the Discord
-                        </Button>
+        <div className={heroStyles.hero}>
+            <Container>
+                <div className={`${heroStyles['hero-container']}`}>
+                    <div className={`${heroStyles['heading-container']}`}>
+                        <Heading style='heading-1' level='1'>
+                            The World’s First NFT Car Club & Marketplace
+                        </Heading>
+                        <div className={`${heroStyles['hero-btns']}`}>
+                            <Button style='primary-btn' link='#'>
+                                Become a Member
+                            </Button>
+                            <Button style='secondary-btn' link='#'>
+                                Join the Discord
+                            </Button>
+                        </div>
                     </div>
+                    <figure className={`${heroStyles['hero-img']}`}>
+                        <Image src={heroImg} height='600' width='600' />
+                    </figure>
                 </div>
-                <figure className={`${heroStyles['hero-img']}`}>
-                    <Image src={heroImg} height='600' width='600' />
-                </figure>
-            </div>
-        </Container>
+            </Container>
+        </div>
     );
 };
 

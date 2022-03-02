12e52dcd0c63adb5549376ec0629f1a29d286feb
Dennis Acosta <email@dennisacosta.me>
2022-03-02 10:40:10 -0500

    Update marketplace content

--- components/Marketplace/Marketplace.js
+++ components/Marketplace/Marketplace.js
@@ -9,7 +9,7 @@ const CONTENT = {
             text: 'Our vision is to open the first NFT marketplace where anyone can buy, sell, or trade digital vehicles in a fully immersive and interactive 3D environment. All transactions are recorded in the Ethereum blockchain using smart contracts to verify and manage the ownership and history of the items.',
         },
         {
-            text: 'The project is also designed with sustainability and long-term utility. 50% of the profits from every item sold in the marketplace will be stored on a locked, secure wallet, that will be used as an expansion fund guided by the community.',
+            text: 'The project is also designed with sustainability and long-term utility. <b style="color:ffea0e;>50% of the profits</b> from every item sold in the marketplace will be stored on a locked, secure wallet, that will be used as an expansion fund guided by the community. ',
         },
         {
             text: 'For the token holder, our goal is to build a utilitarian project that is fun, original, but also valuable, with the potential of creating equity over time.',

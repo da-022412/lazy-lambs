57677f09d8f1639acf7dcb485ac51c4865616ff4
Dennis Acosta <email@dennisacosta.me>
2022-03-02 10:38:30 -0500

    Update club content

--- components/Club/Club.js
+++ components/Club/Club.js
@@ -13,7 +13,7 @@ const CONTENT = {
         },
         {
             heading: 'Driving Experiences',
-            copy: 'Lazy Lamb members are shaped by car culture: its history, its technology, its lifestyle, and most importantly, the thrill of the drive. While our car knowledge is the common bond, it is the passions and pursuits of each Member that makes Lazy Lamb so unique, creating the perfect environment for networking and socializing.',
+            copy: 'Enjoy a wide variety of car-themed events, such as track days, automotive premiers, learning classes, member drives, and other member-only curated experiences that resonate with the automotive enthusiast inside all of us.',
         },
     ],
 };

01fd8113ec8b2982b3214616882bb10165c666a1
Dennis Acosta <email@dennisacosta.me>
2022-03-02 17:15:23 -0500

    Add Form spree

--- components/Layout/Footer/Form/Form.js
+++ components/Layout/Footer/Form/Form.js
@@ -1,14 +1,28 @@
+import { useForm, ValidationError } from '@formspree/react';
+
 import formStyles from './Form.module.scss';
 
+import BodyText from '../../BodyText';
 import Heading from '../../../Heading';
 
 const Form = () => {
+    const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM);
+
+    if (state.succeeded) {
+        return <BodyText>Thanks for your submission!</BodyText>;
+    }
+
     return (
         <div className={`${formStyles['form-container']}`}>
             <Heading style='heading-5' level='5'>
                 Get Updates
             </Heading>
-            <form className={formStyles.form}>
+            <form
+                onSubmit={handleSubmit}
+                action='https://formspree.io/f/mqknbbda'
+                method='POST'
+                className={formStyles.form}
+            >
                 <label htmlFor='submission' hidden>
                     Email
                 </label>
@@ -19,12 +33,19 @@ const Form = () => {
                     type='email'
                     required
                 />
+                <ValidationError
+                    prefix='Email'
+                    field='email'
+                    errors={state.errors}
+                />
                 <button
                     type='submit'
+                    disabled={state.submitting}
                     className={`${formStyles['submit-button']}`}
                 >
                     Submit
                 </button>
+                <ValidationError errors={state.errors} />
             </form>
         </div>
     );

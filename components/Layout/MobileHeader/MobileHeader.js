3915029a91d90e53c3f2f8cdd1d04b0af5e96c2b
Dennis Acosta <email@dennisacosta.me>
2022-03-02 16:16:50 -0500

    Close menu upon clicking link

--- components/Layout/MobileHeader/MobileHeader.js
+++ components/Layout/MobileHeader/MobileHeader.js
@@ -3,12 +3,36 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import headerStyles from '../Header/Header.module.scss';
-
-import MobileNav from './MobileNav';
+import navStyles from './MobileNav.module.scss';
 
 import Logo from '../../../assets/images/logo.svg';
 import Menu from '../../../assets/images/menu.webp';
 import Close from '../../../assets/images/close.webp';
+import Button from '../../Button';
+import SocialIcons from '../../SocialIcons';
+import BodyText from '../../BodyText';
+
+const CONTENT = {
+    links: [
+        {
+            title: 'Club',
+            slug: '/#club',
+        },
+        {
+            title: 'Roadmap',
+            slug: '/#roadmap',
+        },
+        {
+            title: 'Team',
+            slug: '/#team',
+        },
+        {
+            title: 'FAQ',
+            slug: '/faqs',
+        },
+    ],
+    footer: '© 2022 MetaSpeed, Inc. All rights reserved.',
+};
 
 const MobileHeader = () => {
     const [isActive, setIsActive] = useState(false);
@@ -51,7 +75,36 @@ const MobileHeader = () => {
                     </figure>
                 </div>
             </header>
-            {isActive && <MobileNav />}
+            {isActive && (
+                <nav className={navStyles.nav}>
+                    <div className={`${navStyles['nav-container']}`}>
+                        {CONTENT.links.map(({ title, slug }, index) => (
+                            <div
+                                className={`${navStyles['link-container']}`}
+                                key={index}
+                            >
+                                <Link href={slug}>
+                                    <a
+                                        className={`${navStyles['link-text']}`}
+                                        onClick={() => setIsActive(!isActive)}
+                                    >
+                                        {title}
+                                    </a>
+                                </Link>
+                            </div>
+                        ))}
+                        <div className={`${navStyles['btn-container']}`}>
+                            <Button style='primary-btn' link='#'>
+                                Become a Member
+                            </Button>
+                        </div>
+                        <div className={`${navStyles['nav-footer']}`}>
+                            <SocialIcons />
+                            <BodyText>{CONTENT.footer}</BodyText>
+                        </div>
+                    </div>
+                </nav>
+            )}
         </>
     );
 };

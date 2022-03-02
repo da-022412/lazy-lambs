0bdceb230c3455cfc08ca05b9af4fd28d39b3940
Dennis Acosta <email@dennisacosta.me>
2022-03-02 10:45:11 -0500

    Update charity content

--- components/Charity/Charity.js
+++ components/Charity/Charity.js
@@ -1,6 +1,6 @@
 import Columns from '../Columns';
 
-import Img from '../../assets/images/R2R-Header-logo.webp';
+import Img from '../../assets/images/charity.webp';
 
 const CONTENT = {
     title: 'Charity',
@@ -9,13 +9,7 @@ const CONTENT = {
             text: 'We’re not just fun and lifestyle. At Lazy Lamb, we’re also committed to giving back something positive, and doing our part to help take care of others.',
         },
         {
-            text: '<b>As part of our roadmap, we will donate:</b><br><br><ul><li><span style="color:#ffea0e">$200,000</span> to Ride2Revive upon hitting 20% of our roadmap</li><li><span style="color:#ffea0e">$600,000</span> to a non-profit organization selected by majority vote from our members upon hitting 60% of our roadmap</li></ul>',
-        },
-        {
-            text: '<b>Ride2Revive</b> is a non-profit organization that has helped and inspired hundreds of children battling life-threatening illnesses for over ten years by giving them a once-in-a-lifetime exhilarating race car experience, bringing miles of smiles to the kids and their families.',
-        },
-        {
-            text: '<b>Ride2Revive</b> benefits children from various hospitals and organizations, including Joe DiMaggio Children’s Hospital, Holtz Children’s Hospital, Nicklaus Children’s Hospital, and more. To learn more about their work and mission, visit ride2revive.org.',
+            text: '<b>As part of our roadmap, we will donate:</b><br><br><ul><li><span style="color:#ffea0e">$1,000,000</span> to a non-profit organization selected by majority vote from our members upon hitting 60% of our roadmap </li></ul>',
         },
     ],
     img: Img,

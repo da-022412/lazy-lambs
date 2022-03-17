166d7b6280456b98d2508c3fd98d397d4af40977
rzucco <rzucco@gmail.com>
2022-03-16 21:31:51 -0400

    Update Marketplace.js

--- components/Marketplace/Marketplace.js
+++ components/Marketplace/Marketplace.js
@@ -6,7 +6,7 @@ const CONTENT = {
 
     copy: [
         {
-            text: 'Our vision is to open the first NFT marketplace where anyone can buy, sell, or trade digital vehicles in a fully immersive and interactive 3D environment. All transactions are recorded in the Ethereum blockchain using smart contracts to verify and manage the ownership and history of the items.',
+            text: 'Our vision is to open the first in-game marketplace where anyone can buy, sell, or trade digital vehicles in a fully immersive and interactive 3D environment. All transactions are recorded in the Ethereum blockchain using smart contracts to verify and manage the ownership and history of the items.',
         },
         {
             text: 'The project is also designed with sustainability and long-term utility. <span style="color:ffea0e;"><b>50% of the profits</b></span> from every item sold in the marketplace will be stored on a locked, secure wallet, that will be used as an expansion fund guided by the community. ',

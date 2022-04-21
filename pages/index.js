1a4fe610f2c7445f186c4d7e59702682d5da1075
rzucco <rzucco@gmail.com>
2022-04-21 16:57:32 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home() {
             <Divider />
             <Lambs />
             <Divider />
-           // <Game />
-           // <Marketplace />
+            <Game />
+            <Marketplace />
             <Divider />
             <Lambo />
             <Divider />
